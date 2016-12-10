e8b76e21b7bd4f52e9d658f8c4fb1737829d5693
Istvan Balogh <istvan.balogh@innovitech.hu>
2016-12-10 16:13:40 +0100

    Connection fix

--- src/main/java/hu/unideb/inf/tennis/app/TennisApp.java
+++ src/main/java/hu/unideb/inf/tennis/app/TennisApp.java
@@ -1,20 +1,33 @@
 package hu.unideb.inf.tennis.app;
 
+import java.io.IOException;
+
 import javax.xml.xquery.XQConnection;
 import javax.xml.xquery.XQException;
+
+import org.basex.BaseXServer;
+import org.basex.api.client.ClientQuery;
+import org.basex.api.client.ClientSession;
+import org.basex.core.cmd.CreateDB;
 
 import hu.unideb.inf.tennis.connection.Connection;
 
 public class TennisApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		try {
-			XQConnection xqc = Connection.getConnection("localhost", "1984", "bela", "bela");
+			BaseXServer server = new BaseXServer();
+			ClientSession xqc = Connection.getSession("localhost", 1984, "admin", "admin");
+			
+			 xqc.execute(new CreateDB("input", "src/main/resources/tennis.xml"));
+			
+			ClientQuery query = xqc.query("//root"); 
+		    System.out.println(query.execute());
+		    server.stop();
+		      
 		} catch (XQException e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
--- src/main/java/hu/unideb/inf/tennis/connection/Connection.java
+++ src/main/java/hu/unideb/inf/tennis/connection/Connection.java
@@ -1,20 +1,25 @@
 package hu.unideb.inf.tennis.connection;
 
+import java.io.IOException;
+
 import javax.xml.xquery.XQConnection;
 import javax.xml.xquery.XQDataSource;
 import javax.xml.xquery.XQException;
 
+import org.basex.BaseXServer;
+import org.basex.api.client.ClientSession;
+
 import net.xqj.basex.BaseXXQDataSource;
 
 public class Connection {
-
-	public static XQConnection getConnection(String sName, String port, String user, String pw)
-			throws XQException {
-
+	   
+	public static ClientSession getSession(String sName, int port, String user, String pw)
+			throws XQException, IOException {
+		
 		if (sName == null) {
 			throw new NullPointerException("Servername is null!");
 		}
-		if (port == null) {
+		if (port == 0) {
 			throw new NullPointerException("Port is null!");
 		}
 		if (user == null) {
@@ -23,14 +28,9 @@ public class Connection {
 		if (pw == null) {
 			throw new NullPointerException("Password is null!");
 		}
+		ClientSession session = new ClientSession(sName, port, user, pw);
 
-		XQDataSource ds = new BaseXXQDataSource();
-		ds.setProperty("serverName", sName);
-		ds.setProperty("port", port);
-		ds.setProperty("user", user);
-		ds.setProperty("password", pw);
-
-		return ds.getConnection();
+		return session;
 	}
 
 }
